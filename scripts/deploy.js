4bf34ee2b0c3dd1e7e85b9cee1735cd721e88560
Kingsley Victor <javaprodigy56@gmail.com>
2023-03-27 15:28:25 +0100

    one kind update laidis

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,14 +2,14 @@ const { ethers } = require("hardhat");
 
 (async () => {
   try {
-    const TokenFactory = await ethers.getContractFactory("BriseShippuden");
+    const TokenFactory = await ethers.getContractFactory("LittleBitgertWarrior");
     console.log("Deploying...");
     let token = await TokenFactory.deploy(
-      "Brise Shippuden",
-      "BRISE-DEN",
-      ethers.utils.parseUnits("4000000000000", 18),
+      "Little Bitgert Warrior",
+      "LBW",
+      ethers.utils.parseUnits("60000000000", 18),
       "0xDCcB4aC22E5CaDd542B693DEfb41Aea5553E4749",
-      8
+      5
     );
     token = await token.deployed();
     console.log(token.address);
